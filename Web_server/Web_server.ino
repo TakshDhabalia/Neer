3870ddc4324dcc62a0b86b9e0435c441dabe8625
Taksh <dhabalia.taksh@gmail.com>
2024-06-25 21:49:21 +0530

    code updates for server and client

--- Web_server/Web_server.ino
+++ Web_server/Web_server.ino
@@ -1,112 +1,108 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
 
-// Replace with your network credentials
 const char* ssid = "taksh";
 const char* password = "12345678";
-// Set web server port number to 80
-WiFiServer server(80);
+const char* thingspeakServer = "api.thingspeak.com";
+const char* apiKey = "0EXT8TLC3UWY00DS";
 
-// Variable to store the HTTP request
-String header;
-
-// Variable to store the distance
+ESP8266WebServer server(80);
 float distance = 0;
 
 void setup() {
   Serial.begin(115200);
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  delay(100); // Delay added for Serial Monitor to initialize
+  
+  // Connect to Wi-Fi
+  Serial.print("Connecting to Wi-Fi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+  Serial.println("WiFi connected");
+
+  // Start the HTTP server
+  server.on("/update", handleUpdate);
+  server.begin();  // Start the server
+
+  // Print the IP address where the server can be accessed
+  Serial.print("HTTP server started. Access it at: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
+  Serial.println("Ready to receive updates.");
 }
 
 void loop() {
-  WiFiClient client = server.available();   // Listen for incoming clients
+  server.handleClient();
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
+  // Update ThingSpeak every second if distance is greater than 0
+  if (distance > 0) {
+    if (updateThingSpeak(distance)) {
+      distance = 0; // Reset distance after successful update
+      Serial.println("ThingSpeak update successful");
+    } else {
+      Serial.println("ThingSpeak update failed");
+    }
+  }
 
-            // Check if the request contains the distance data
-            if (header.indexOf("GET /update?distance=") >= 0) {
-              int index = header.indexOf("distance=") + 9;
-              String distanceString = header.substring(index);
-              distanceString = distanceString.substring(0, distanceString.indexOf(" "));
-              distance = distanceString.toFloat();
-              Serial.print("Updated Distance: ");
-              Serial.println(distance);
-            }
+  delay(1000);
+}
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style>");
-            client.println("<script>");
-            client.println("setInterval(function() {");
-            client.println("  var xhttp = new XMLHttpRequest();");
-            client.println("  xhttp.onreadystatechange = function() {");
-            client.println("    if (this.readyState == 4 && this.status == 200) {");
-            client.println("      document.getElementById(\"distance\").innerHTML = this.responseText;");
-            client.println("    }");
-            client.println("  };");
-            client.println("  xhttp.open(\"GET\", \"/distance\", true);");
-            client.println("  xhttp.send();");
-            client.println("}, 1000);"); // Update every 1 second
-            client.println("</script></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display the distance
-            client.println("<p>Distance: <span id=\"distance\">" + String(distance) + "</span> cm</p>");
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+void handleUpdate() {
+  if (server.hasArg("distance")) {
+    distance = server.arg("distance").toFloat();
+    server.send(200, "text/plain", "Distance received");
+    Serial.print("Distance received: ");
+    Serial.println(distance);
+  } else {
+    server.send(400, "text/plain", "Invalid request");
+    Serial.println("Invalid request");
+  }
+}
+
+bool updateThingSpeak(float distance) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    if (client.connect(thingspeakServer, 80)) {
+      String postStr = "GET /update?api_key=";
+      postStr += apiKey;
+      postStr += "&field1=";
+      postStr += String(distance);
+      postStr += " HTTP/1.1\r\n";
+      postStr += "Host: ";
+      postStr += thingspeakServer;
+      postStr += "\r\n";
+      postStr += "Connection: close\r\n\r\n";
+
+      client.print(postStr);
+      Serial.println("Data sent to ThingSpeak");
+
+      // Wait for response from ThingSpeak
+      unsigned long timeout = millis();
+      while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+          Serial.println("No response from ThingSpeak");
+          client.stop();
+          return false;
         }
       }
+
+      // Read the response from ThingSpeak
+      String response = client.readStringUntil('\r');
+      Serial.print("Response from ThingSpeak: ");
+      Serial.println(response);
+
+      client.stop();
+      return true;
+    } else {
+      Serial.println("Connection to ThingSpeak failed");
+      return false;
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+  } else {
+    Serial.println("WiFi not connected");
+    return false;
   }
 }
